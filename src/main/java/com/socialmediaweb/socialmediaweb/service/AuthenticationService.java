4af89baeb83b2678b900a447568a064e12bf5661
silasyeak <silasyeak@gmail.com>
2023-06-14 12:07:44 +0800

    Update AuthenticationService.java

--- src/main/java/com/socialmediaweb/socialmediaweb/service/AuthenticationService.java
+++ src/main/java/com/socialmediaweb/socialmediaweb/service/AuthenticationService.java
@@ -1,8 +1,37 @@
 package com.socialmediaweb.socialmediaweb.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.socialmediaweb.socialmediaweb.entities.Users;
+import com.socialmediaweb.socialmediaweb.repository.UserRepository;
 
 @Service
 public class AuthenticationService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public Users saveUser(Users users) {
+		return repository.save(users);
+	}
+	
+	public List<Users> SaveUsers(List<Users> users) {
+		return repository.saveAll(users);
+	}
+	
+	public List<Users> getUsers(){
+		return repository.findAll();
+	}
+	
+	public Users getUsersById(int id) { 
+		return repository.findById(id).orElse(null);
+	}
+	
+	public Users getUsersByUsername(String username) { 
+		return repository.findByName(username);
+	}
 	
 }
