ed41121acbd7be965a82274bc6b8d2816be8431c
silasyeak <silasyeak@gmail.com>
2023-06-15 01:11:42 +0800

    added userroles, foreign keys

--- src/main/java/com/socialmediaweb/socialmediaweb/entities/Post.java
+++ src/main/java/com/socialmediaweb/socialmediaweb/entities/Post.java
@@ -26,43 +26,66 @@ public class Post {
 		
 	}
 	
-	public Post(int post_id, String content, String image, Date created_on) {
+
+	public Post(int post_id, String content, String image, Date created_on, Users user) {
 		super();
 		this.post_id = post_id;
 		this.content = content;
 		this.image = image;
 		this.created_on = created_on;
+		this.user = user;
 	}
+
+
 	public int getPost_id() {
 		return post_id;
 	}
+
+
 	public void setPost_id(int post_id) {
 		this.post_id = post_id;
 	}
+
+
 	public String getContent() {
 		return content;
 	}
+
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
+
 	public String getImage() {
 		return image;
 	}
+
+
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+
 	public Date getCreated_on() {
 		return created_on;
 	}
+
+
 	public void setCreated_on(Date created_on) {
 		this.created_on = created_on;
 	}
 
-	@Override
-	public String toString() {
-		return "Post [post_id=" + post_id + ", content=" + content + ", image=" + image + ", created_on=" + created_on
-				+ "]";
+
+	public Users getUser() {
+		return user;
 	}
+
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
 	
 	
 	
