7ab2903e2c64c9eac4322586001c8a88d5aaaf31
sabrina <nursabrinaosm@gmail.com>
2023-06-14 13:02:16 +0800

    Role update

--- src/main/java/com/socialmediaweb/socialmediaweb/entities/Role.java
+++ src/main/java/com/socialmediaweb/socialmediaweb/entities/Role.java
@@ -1,6 +1,14 @@
 package com.socialmediaweb.socialmediaweb.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int role_id;
 	private String role;
 	
