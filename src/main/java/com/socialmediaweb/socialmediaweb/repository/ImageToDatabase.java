95ba6edb00193abfdc8ddb741606da13baffca2b
silasyeak <silasyeak@gmail.com>
2023-06-16 15:19:09 +0800

    ImageToDB

--- src/main/java/com/socialmediaweb/socialmediaweb/repository/ImageToDatabase.java
+++ src/main/java/com/socialmediaweb/socialmediaweb/repository/ImageToDatabase.java
@@ -14,18 +14,19 @@ public class ImageToDatabase {
         String username = "root"; // Replace with your MySQL username
         String password = "4GgcT6>bUoo>"; // Replace with your MySQL password
 
-        String imagePath = "/socialmedia-web/src/main/resources/profilePics"; // Replace with the path to your image file
+        String imagePath = "/socialmedia-web/src/main/resources/profilePics/1.jpg"; // Replace with the path to your image file
 
         try (Connection conn = DriverManager.getConnection(url, username, password)) {
             File imageFile = new File(imagePath);
             FileInputStream fis = new FileInputStream(imageFile);
 
-            String sql = "INSERT INTO images (image_data) VALUES (?)";
+            String sql = "UPDATE users SET profile_picture = ? WHERE id = ?"; // Modify the SQL statement to update the profile_picture column in the users table
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setBinaryStream(1, fis, (int) imageFile.length());
+            statement.setInt(2, 1); // Replace 1 with the actual ID of the user you want to update
             statement.executeUpdate();
 
-            System.out.println("Image stored in the database successfully.");
+            System.out.println("Profile picture stored in the database successfully.");
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
